1394c7dee5949fa1356b51eaca33f28b1921695c
nesto2001 <tanlong6121@gmail.com>
2023-04-06 13:34:54 +0700

    fix deactivate

--- src/containers/AdminContainer/AccountContainer/AccountsContainer.tsx
+++ src/containers/AdminContainer/AccountContainer/AccountsContainer.tsx
@@ -32,7 +32,7 @@ const columns: ColumnsType<AccountModel> = [
         dataIndex: '',
         key: 'x',
         render: (_, record) => {
-            return record.status ? <a className="text-red">De activate</a> : <a className="text-primary">Activate</a>;
+            return record.status ? <a className="text-red">Deactivate</a> : <a className="text-primary">Activate</a>;
         },
     },
 ];
