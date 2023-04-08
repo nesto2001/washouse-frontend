cfaac31ed3a786922cc42f20941c8bfefa887586
nesto2001 <tanlong6121@gmail.com>
2023-04-09 02:54:23 +0700

    fix config tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,19 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors');
+
 module.exports = {
     content: ['./src/**/*.{js,jsx,ts,tsx}', './node_modules/tailwind-datepicker-react/dist/**/*.js'],
     theme: {
         extend: {},
         colors: {
+            transparent: 'transparent',
+            current: 'currentColor',
+            black: colors.black,
+            gray: colors.gray,
+            emerald: colors.emerald,
+            white: colors.white,
+            indigo: colors.indigo,
+            yellow: colors.yellow,
             primary: '#396afc',
         },
     },
